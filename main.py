9f1527e7b25fd4af87f0d6cc4fae6249e7c8ada8
harun-a1 <43985436+harun-a1@users.noreply.github.com>
2019-11-09 12:47:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    ChuckNorris()
+    app.run()
